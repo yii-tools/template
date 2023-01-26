50e2a69e220df6043ec67c2ecca3e5e5a2a86cb6
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-01-26 12:36:57 -0300

    Update template. (#3)

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Yii\Extension\Widget\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Template\Example;
 
 final class ExampleTest extends TestCase
 {
     public function testExample(): void
     {
-        $this->assertTrue(true);
+        $example = new Example();
+
+        $this->assertTrue($example->getExample());
     }
 }
